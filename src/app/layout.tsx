b01eb4edfd69edfeda86a626d7cb8aab0740ad6e
kiyoonj <rldbs2002@naver.com>
2024-01-26 14:40:56 +0900

    vercel deploy2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,20 +4,17 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import AuthProvider from "./contexts/AuthProvider";
 import "./globals.css";
-import { useEffect } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 import MuiTheme from "./theme/MuiTheme";
 import GoogleAnalytics from "./components/GoogleAnalytics";
 
-
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  
   return (
     <html lang="en">
       <body className={inter.className}>
@@ -32,13 +29,13 @@ export default function RootLayout({
   );
 }
 
-export const metadata: Metadata = {
-  title: "Kgoods | Ship Worldwide | Free Korean Address",
-  description:
-    "Buy KPOP, Korean skincare, and any products from Korean shopping sites. Use our free Korean address, consolidation, forwarding, and personal shopper!",
-  openGraph: {
-    title: "Kgoods | Ship Worldwide",
-    description:
-      "Buy products from Korean shopping sites. Use our free Korean address, consolidation, forwarding, and personal shopper!",
-  },
-};
+// export const metadata: Metadata = {
+//   title: "Kgoods | Ship Worldwide | Free Korean Address",
+//   description:
+//     "Buy KPOP, Korean skincare, and any products from Korean shopping sites. Use our free Korean address, consolidation, forwarding, and personal shopper!",
+//   openGraph: {
+//     title: "Kgoods | Ship Worldwide",
+//     description:
+//       "Buy products from Korean shopping sites. Use our free Korean address, consolidation, forwarding, and personal shopper!",
+//   },
+// };
